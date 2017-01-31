fd6b504b689c4c5109725756df9befb5b8937281
Geethanjhali Madanagopal <g.madanagopal@accenture.com>
2017-01-31 17:51:08 +0530

    Commit

--- TestJenkinsDotnet/TestJenkinsDotnet/Controllers/ValuesController.cs
+++ TestJenkinsDotnet/TestJenkinsDotnet/Controllers/ValuesController.cs
@@ -3,38 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using JenkinsDotNet;
+using System.Text;
+using System.IO;
+using System.Xml.Linq;
+using WebApplication1.Models;
 
-namespace TestJenkinsDotnet.Controllers
+
+namespace JenkinsAPI.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class ValuesController : ApiController
+
     {
         // GET api/values
-        public IEnumerable<string> Get()
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+
+        // GET api/values
+
+        JenkinsDotNet.JenkinsServer _jenkinsServer = new JenkinsDotNet.JenkinsServer("http://10.233.4.210:8080", "g.madanagopal", "9b2d406a6c019fe2a4cdff12da0814b3");
+        HttpAdapter _httpAdapter = new HttpAdapter();
+
+        // GET api/values        
+        public async Task<IHttpActionResult> GetJobDetails()
         {
-            return new string[] { "value1", "value2" };
+            JenkinsDotNet.Model.Node _node = new JenkinsDotNet.Model.Node();
+            _node = await _jenkinsServer.GetNodeDetailsAsync();
+            return Ok(_node.Jobs);
         }
 
-        // GET api/values/5
-        public string Get(int id)
+        // GET api/values/{projectToken}
+
+        public IHttpActionResult GetTriggerBuild(string projectToken)
         {
-            return "value";
+            //_httpAdapter.Infrastructure.HttpAdapter _httpAdapter = new Jenkins_CSharp_Api.Infrastructure.HttpAdapter();
+            string response = _httpAdapter.TriggerBuild(projectToken);
+            return Ok(response);
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
         // DELETE api/values/5
         public void Delete(int id)
         {
         }
+
+
+
     }
 }
+
---
+++ TestJenkinsDotnet/TestJenkinsDotnet/Models/HttpAdapter.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Xml.Linq;
+
+namespace WebApplication1.Models
+{
+    public class HttpAdapter
+    {
+        static string server = "http://localhost:8080/";
+        static string apiToken = "9b2d406a6c019fe2a4cdff12da0814b3";
+        static string username = "g.madanagopal";
+        static string responseFromServer = string.Empty;
+
+        public string TriggerBuild(string projectToken)
+        {
+            string response = JenkinsTriggerBuild("http://localhost:8080/job/JenkinsApp/build?token=" + projectToken, projectToken);
+            return response;
+        }
+
+        public static string JenkinsTriggerBuild(string url, string projectToken)
+        {
+            string nextBuildNumber = GetNextBuildNumber(projectToken);
+
+            var httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(url);
+            httpWebRequest.ContentType = "application/x-www-form-urlencoded";
+            httpWebRequest.Method = WebRequestMethods.Http.Get;
+            byte[] credentialBuffer = new UTF8Encoding().GetBytes(username + ":" + apiToken);
+
+            httpWebRequest.Headers["Authorization"] = "Basic " + Convert.ToBase64String(credentialBuffer);
+            httpWebRequest.PreAuthenticate = true;
+
+            try
+            {
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                var isbuilding = WaitUntilBuildFinish(projectToken, nextBuildNumber);
+                if (!isbuilding)
+                    responseFromServer = GetBuildResult(projectToken, nextBuildNumber);
+            }
+            catch (WebException excp)
+            {
+                using (var streamReader = new StreamReader(excp.Response.GetResponseStream()))
+                {
+                    responseFromServer = streamReader.ReadToEnd();
+                }
+            }
+            return responseFromServer;
+        }
+
+        public static bool IsProjectBuilding(string projectToken, string nextBuildNumber)
+        {
+            //http://localhost:8080/job/JenkinsApp/29/api/xml
+
+            bool isBuilding = false;
+            string apiUrl = server + "job/" + projectToken + "/" + nextBuildNumber + "/api/xml";
+            string isBuildingStr = GetXmlNodeValue(apiUrl, "building");
+            isBuilding = bool.Parse(isBuildingStr);
+            return isBuilding;
+        }
+
+        public static string GetXmlNodeValue(string apiUrl, string xmlNodeName)
+        {
+            IEnumerable<XElement> foundElemenets = GetXMLNodeValue(apiUrl, xmlNodeName);
+            if (foundElemenets.Count() == 0)
+            {
+                throw new Exception(
+                string.Format("No elements were found for node {0}", xmlNodeName));
+            }
+            string elementValue = foundElemenets.First().Value;
+            return elementValue;
+        }
+
+        public static string GetBuildResult(string projectToken, string nextBuildNumber)
+        {
+            string buildResult = string.Empty;
+            try
+            {
+                string apiUrl = server + "job/" + projectToken + "/" + nextBuildNumber + "/api/xml";
+                buildResult = GetXmlNodeValue(apiUrl, "result");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return buildResult;
+        }
+
+        public static string GetNextBuildNumber(string jobName)
+        {
+            string nextBuildNumber = string.Empty;
+            try
+            {
+                string apiUrl = server + "job/" + jobName + "/api/xml";
+                nextBuildNumber = GetXmlNodeValue(apiUrl, "nextBuildNumber");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return nextBuildNumber;
+        }
+
+        public static IEnumerable<XElement> GetXMLNodeValue(string apiUrl, string xmlNodeName)
+        {
+            IEnumerable<XElement> foundElemenets;
+
+            try
+            {
+                var httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(apiUrl);
+                httpWebRequest.ContentType = "application/xml";
+                httpWebRequest.Method = WebRequestMethods.Http.Get;
+                byte[] credentialBuffer = new UTF8Encoding().GetBytes(username + ":" + apiToken);
+
+                httpWebRequest.Headers["Authorization"] = "Basic " + Convert.ToBase64String(credentialBuffer);
+                httpWebRequest.PreAuthenticate = true;
+
+                if (httpWebRequest != null)
+                {
+                    using (HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse())
+                    {
+                        using (Stream stream = response.GetResponseStream())
+                        {
+                            XDocument document = XDocument.Load(stream);
+                            XElement root = document.Root;
+                            foundElemenets = from element in root.Descendants(xmlNodeName) select element;
+                        }
+                    }
+                }
+                else
+                    foundElemenets = null;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return foundElemenets;
+        }
+
+        public static bool WaitUntilBuildFinish(string projectToken, string nextBuildNumber)
+        {
+            bool shouldContinue = false;
+            string buildStatus = string.Empty;
+
+            //Wait for build creation
+            Thread.Sleep(10000);
+
+            do
+            {
+                bool isProjectBuilding = IsProjectBuilding(projectToken, nextBuildNumber);
+                if (isProjectBuilding)
+                {
+                    shouldContinue = true;
+                    Debug.WriteLine("Waits 5 seconds before the new check if the build is completed...");
+                    Thread.Sleep(5000);
+                }
+                else
+                {
+                    shouldContinue = false;
+                }
+            }
+            while (shouldContinue);
+
+            return shouldContinue;
+        }
+
+    }
+}
